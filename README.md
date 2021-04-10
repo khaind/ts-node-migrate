8a68604dfd44a5aa1848066c02dfeb9b4e38f14f
Khai Nguyen <khaindk@gmail.com>
2021-04-10 09:59:32 +0700

    feat: add template cli

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ts-node-migrate
-Typescript database migration cli
+A generic typescript database migration cli for potentially multiple databases supported
+
+![npm type definitions](https://img.shields.io/npm/types/typescript)
+![GitHub last commit](https://img.shields.io/github/last-commit/khaind/ts-node-migrate)
+
+Support databases:
+- Mongodb (In Progress)
+- Mysql (TBD)
+
+## Installation
+
+## Usage: 
+
+````
+tsnm [options] [command]
+
+Options:
+  -h, --help      display help for command
+
+Commands:
+  init            Creates the migrations directory and configuration file
+  new             Creates new migration template
+  status          Verify migration status
+  up              Doing migration forward
+  down            Doing migration backward
+  help [command]  display help for command
+```` 
--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,45 @@
-console.log('init');
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .command('init')
+  .description('Creates the migrations directory and configuration file')
+  .action(() => {
+    console.log('Initialized');
+    // TODO init();
+  });
+
+program
+  .command('new')
+  .description('Creates new migration template')
+  .action(() => {
+    console.log('New migration');
+    // TODO
+  });
+
+program
+  .command('status', { isDefault: true })
+  .description('Verify migration status')
+  .action(() => {
+    console.log('Status');
+    // TODO
+  });
+
+program
+  .command('up')
+  .description('Doing migration forward')
+  .action(() => {
+    console.log('Up migration');
+    // TODO
+  });
+
+program
+  .command('down')
+  .description('Doing migration backward')
+  .action(() => {
+    console.log('Down migration');
+    // TODO
+  });
+
+program.parse(process.argv);
